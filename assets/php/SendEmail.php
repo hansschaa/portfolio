d5e4c2283275d2503b4e8285b3e914e2dbf3b7f0
hansschaa <hansschaadev@gmail.com>
2020-08-28 04:09:25 -0400

    Mail section complete 2

--- assets/php/SendEmail.php
+++ assets/php/SendEmail.php
@@ -1,5 +1,5 @@
 <?php
 if($_POST["message"]) {
-    mail("your@email.address",  $_POST["name"], $_POST["message"], $_POST["email"]);
+    mail("hansschaadev@gmail.com",  $_POST["name"], $_POST["message"], $_POST["email"]);
 }
 ?>

c16e75b3e9852c650e47870c8980ef3b8807cbad
hansschaa <hansschaadev@gmail.com>
2020-08-28 04:16:25 -0400

    Mail section complete 3

--- assets/php/SendEmail.php
+++ assets/php/SendEmail.php
@@ -1,5 +1,10 @@
 <?php
-if($_POST["message"]) {
-    mail("hansschaadev@gmail.com",  $_POST["name"], $_POST["message"], $_POST["email"]);
-}
+$para      = 'hansschaadev@gmail.com';
+$titulo    = 'El título';
+$mensaje   = 'Hola';
+$cabeceras = 'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($para, $titulo, $mensaje, $cabeceras);
 ?>

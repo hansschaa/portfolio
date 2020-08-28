c118419f655768d3d95bd582e9af2d62eca005f1
hansschaa <hansschaadev@gmail.com>
2020-08-28 04:36:15 -0400

    Mail section complete 7

--- assets/php/SendEmail.php
+++ assets/php/SendEmail.php
@@ -1,12 +1,12 @@
 <?php
 
 // the message
-$msg = "First line of text\nSecond line of text";
+$msg = $POST_["message"];
 
 // use wordwrap() if lines are longer than 70 characters
 $msg = wordwrap($msg,70);
 
 // send email
-mail("hansschaadev@gmail.com","My subject",$msg);
+mail("hansschaadev@gmail.com","Pagina web",$msg);
 
 ?>
